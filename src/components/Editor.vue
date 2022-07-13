901c61d289c18f29f5777c29837285bb6d78399d
felix <vip.zatebe@mail.ru>
2022-07-13 17:13:12 +0300

    Minor Bugfix

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="editor">
-    <vue-editor v-model="code" @text-change="lineEditorCode=code"></vue-editor>
-    <codemirror v-model="lineEditorCode" :options="cmOptions" @blur="code=lineEditorCode" />
+    <div class="editorPanel">
+      <vue-editor v-model="code" @text-change="guiToLine"></vue-editor>
+    </div>
+    <div class="editorPanel">
+      <codemirror v-model="lineEditorCode" :options="cmOptions" @blur="lineToGui()" ref="editor"/>
+    </div>
   </div>
 </template>
 
@@ -9,6 +13,8 @@
 import {codemirror} from 'vue-codemirror';
 import {VueEditor} from "vue2-editor";
 import 'codemirror/lib/codemirror.css'
+
+import 'codemirror/mode/css/css.js'
 import 'codemirror/mode/htmlmixed/htmlmixed.js'
 
 export default {
@@ -26,6 +32,22 @@ export default {
       }
     }
   },
+  computed: {
+    codemirror_ref: function () {
+      return this.$refs.editor.codemirror
+    }
+  },
+  methods: {
+    // format: function () {
+    //   return this.codemirror_ref.autoFormatRange(this.codemirror_ref.getCursor(true), this.codemirror_ref);
+    // },
+    guiToLine: function () {
+      this.lineEditorCode = this.code;
+    },
+    lineToGui: function () {
+      this.code = this.lineEditorCode.replaceAll('\n', '');
+    }
+  },
   components: {
     VueEditor,
     codemirror
@@ -37,5 +59,23 @@ export default {
 .editor {
   display: flex;
   flex-direction: row;
+  --editor-height: 800px
+}
+
+.editorPanel {
+  width: 50%!important;
+  height: var(--editor-height) !important;
+  max-height: var(--editor-height) !important;
+  min-height: var(--editor-height) !important;
+  display: flex;
+  flex-direction: column;
+}
+
+.CodeMirror {
+  height: var(--editor-height);
+}
+
+.quillWrapper {
+  height: var(--editor-height) !important;
 }
 </style>
